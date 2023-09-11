d3dc5fbce83405b75f24a159a529643e9f4e9d2c
Edgar Haond <edgar.haond@gmail.com>
2023-09-11 23:22:27 +0200

    please work

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
 import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
-import type { Database } from '@/types_db'
+import { Database } from './lib/database.types'
 
 export async function middleware(req: NextRequest) {
   const res = NextResponse.next()
