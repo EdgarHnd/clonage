bd2bb39e196741effbd5b72e5a1628f5f7d6066c
Edgar Haond <edgar.haond@gmail.com>
2023-09-14 22:19:01 +0200

    lowercase is better

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,10 @@ export default function LandingPage() {
       <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
         <div className="text-center">
           <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            Generate unlimited videos of yourself
+            generate unlimited videos of yourself
           </h1>
           <p className="mt-6 text-lg leading-8 text-gray-600">
-            Clone your voice, add your face, and generate videos from any text
+            clone your voice, add your face, and generate videos from any text
           </p>
           <p className="mt-6 text-lg leading-8 text-gray-600">
             <strong> (yes even in other languages 🤯)</strong>
