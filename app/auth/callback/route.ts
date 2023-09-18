559def4118d1715e2971b000b4e7f905f25ff0de
Edgar Haond <edgar.haond@gmail.com>
2023-09-18 11:39:47 +0200

    logging auth error

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -1,19 +1,26 @@
-import { Database } from '@/lib/database.types'
-import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
-import { cookies } from 'next/headers'
-import { NextResponse } from 'next/server'
+import { Database } from '@/lib/database.types';
+import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs';
+import { cookies } from 'next/headers';
+import { NextResponse } from 'next/server';
 
-import type { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const requestUrl = new URL(request.url)
-  const code = requestUrl.searchParams.get('code')
+  try {
+    const requestUrl = new URL(request.url);
+    const code = requestUrl.searchParams.get('code');
 
-  if (code) {
-    const supabase = createRouteHandlerClient<Database>({ cookies })
-    await supabase.auth.exchangeCodeForSession(code)
+    if (code) {
+      const supabase = createRouteHandlerClient<Database>({ cookies });
+      const { error } = await supabase.auth.exchangeCodeForSession(code);
+      if (error) {
+        console.error('Error exchanging code for session:', error.message);
+      }
+    }
+
+    // URL to redirect to after sign in process completes
+    return NextResponse.redirect(requestUrl.origin);
+  } catch (error: any) {
+    console.error('Error in GET handler:', error.message);
   }
-
-  // URL to redirect to after sign in process completes
-  return NextResponse.redirect(requestUrl.origin)
 }
