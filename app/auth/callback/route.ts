d6c5e21c7777054322805c36b36705c22a2ac26a
Edgar Haond <edgar.haond@gmail.com>
2023-09-18 11:53:55 +0200

    force dynamic on auth route

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -5,6 +5,8 @@ import { NextResponse } from 'next/server'
 
 import type { NextRequest } from 'next/server'
 
+export const dynamic = 'force-dynamic'
+
 export async function GET(request: NextRequest) {
   const requestUrl = new URL(request.url)
   const code = requestUrl.searchParams.get('code')
