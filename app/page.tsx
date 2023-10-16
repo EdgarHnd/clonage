6e23f29b352d15689a43d02559750a48ca2fbc99
Edgar Haond <edgar.haond@gmail.com>
2023-10-16 10:38:30 +0200

    this wasn't the problem

--- app/page.tsx
+++ app/page.tsx
@@ -1,53 +1,11 @@
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { getSession } from '@/app/supabase-server';
+import { redirect } from 'next/navigation';
 
-export default function LandingPage() {
-  return (
-    <div className="relative isolate px-6 lg:px-8">
-      <div
-        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
-          }}
-        />
-      </div>
-      <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl">
-            generate unlimited videos of yourself
-          </h1>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            clone your voice, add your face, and generate videos from any text
-          </p>
-          <p className="mt-6 text-lg leading-8 text-gray-600">
-            <strong> (yes even in other languages 🤯)</strong>
-          </p>
-          <div className="mt-10 flex items-center justify-center gap-x-6">
-            <Link href="/signin">
-              <Button variant="secondary" size="lg">
-                get started
-              </Button>
-            </Link>
-          </div>
-        </div>
-      </div>
-      <div
-        className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
-        aria-hidden="true"
-      >
-        <div
-          className="relative left-[calc(50%+3rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-[72.1875rem]"
-          style={{
-            clipPath:
-              'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)'
-          }}
-        />
-      </div>
-    </div>
-  );
+export default async function LandingPage() {
+  const session = await getSession();
+  if (session) {
+    return redirect('/generate');
+  } else {
+    return redirect('/signin');
+  }
 }
