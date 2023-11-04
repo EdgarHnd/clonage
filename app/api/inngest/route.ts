a87c9c4e6b47cbce99154537c0aee61dff5adf8c
Edgar Haond <edgar.haond@gmail.com>
2023-11-04 10:34:09 +0100

    add inngest function to api

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,14 +1,14 @@
 import { inngest } from '@/lib/inngest/client';
-import { generateVideo, helloWorld } from '@/lib/inngest/functions';
+import {
+  generateVideo,
+  helloWorld,
+  onGenerationCompletion
+} from '@/lib/inngest/functions';
 import { serve } from 'inngest/next';
 
 export const maxDuration = 300;
 // Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
-  functions: [
-    helloWorld,
-    generateVideo,
-  ],
+  functions: [helloWorld, generateVideo, onGenerationCompletion]
 });
-
